a94517e0986da4b07e421634098ad71405af7d8e
luca-ronca <luca.ronca@hootsuite.com>
2022-12-05 22:39:02 +0100

    Update module path

--- cmd/webapi/main.go
+++ cmd/webapi/main.go
@@ -28,17 +28,18 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
-	"git.sapienzaapps.it/fantasticcoffee/fantastic-coffee-decaffeinated/service/api"
-	"git.sapienzaapps.it/fantasticcoffee/fantastic-coffee-decaffeinated/service/database"
-	"git.sapienzaapps.it/fantasticcoffee/fantastic-coffee-decaffeinated/service/globaltime"
-	"github.com/ardanlabs/conf"
-	_ "github.com/mattn/go-sqlite3"
-	"github.com/sirupsen/logrus"
 	"math/rand"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
+
+	"github.com/ardanlabs/conf"
+	"github.com/lucaronca/wasa-homework/service/api"
+	"github.com/lucaronca/wasa-homework/service/database"
+	"github.com/lucaronca/wasa-homework/service/globaltime"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/sirupsen/logrus"
 )
 
 // main is the program entry point. The only purpose of this function is to call run() and set the exit code if there is
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module git.sapienzaapps.it/fantasticcoffee/fantastic-coffee-decaffeinated
+module github.com/lucaronca/wasa-homework
 
 go 1.17
 
